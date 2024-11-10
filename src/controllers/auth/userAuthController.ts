b9dd0db3140403079b721c6530b51effc3fd86a7
dev001 <jigarborde2@gmail.com>
2024-11-10 11:16:24 +0530

    Update userAuthController.ts

--- src/controllers/auth/userAuthController.ts
+++ src/controllers/auth/userAuthController.ts
@@ -71,7 +71,6 @@ export const userGoogleLogin = async (req: Request, res: Response, next: NextFun
 
         res.cookie('Authtoken', jwtToken, {
             httpOnly: true,
-            secure: process.env.NODE_ENV === 'production',
             maxAge: 24 * 60 * 60 * 1000, // 24 hours
         });
 
